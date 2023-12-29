77b6914b438ee5988e36cc7e9132b9699dbb484b
gcornejo441 <gcornejo441@gmail.com>
2023-12-28 22:06:24 -0500

    crud ops to cli proj

--- ViewModels/AddShopViewModel.cs
+++ ViewModels/AddShopViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Fetch.Model.Entities;
 using Fetch.Shared;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,20 +14,32 @@ namespace Fetch.ViewModels
 {
     public partial class AddShopViewModel : BaseViewModel
     {
-        public ObservableCollection<SandwichShop> SandwichShops { get; set; } = new();
-        public AddShopViewModel()
-        {
-
-        }
-
+        [ObservableProperty]
+        Shop currentShop;
         [ObservableProperty]
         string defaultPageTitle;
-
+        
+        public AddShopViewModel()
+        {
+            CurrentShop = new Shop();
+        }
 
 
         [RelayCommand]
         void PostNewShop()
         {
+            var Shop = CurrentShop.Shop1;
+            var Address = CurrentShop.Locations;
+            var Specialty = CurrentShop.Specialty;
+
+            // Validate input data
+            if (string.IsNullOrWhiteSpace(Shop) || Address != null || string.IsNullOrWhiteSpace(Specialty))
+            {
+                // Handle validation failure (e.g., notify the user)
+                return;
+            }
+
+            defaultPageTitle = Shop;
 
         }
     }
